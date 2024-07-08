b728b98047b554eacb92bdf875714c6abc483f3a
mrTebrom <mr.tebron@gmail.com>
2024-07-08 18:03:09 +0500

    swager dto

--- src/role/dto/create-role.dto.ts
+++ src/role/dto/create-role.dto.ts
@@ -1,12 +1,18 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateRoleDto {
   // Поле value должно быть строкой и не должно быть пустым
+  @ApiProperty({ example: 'admin', description: 'Название роли' })
   @IsString({ message: 'Значение роли должно быть строкой' })
   @IsNotEmpty({ message: 'Значение роли не должно быть пустым' })
   value: string; // Название роли
 
   // Поле description должно быть строкой
+  @ApiProperty({
+    example: 'Роль администратора с полным доступом',
+    description: 'Описание роли',
+  })
   @IsString({ message: 'Описание роли должно быть строкой' })
   description: string; // Описание роли
 }
