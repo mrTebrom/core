3800f78c0c409726d76ace2285278d60a3d82b89
mrTebrom <mr.tebron@gmail.com>
2024-07-10 12:09:46 +0500

    add user model

---
+++ src/user/entity/user.entity.ts
@@ -0,0 +1,63 @@
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  PrimaryKey,
+  AutoIncrement,
+} from 'sequelize-typescript';
+import { ApiProperty } from '@nestjs/swagger';
+import { CreateUserDto } from '../dto/create-user.dto';
+
+@Table({
+  tableName: 'users',
+  timestamps: true,
+})
+export class User extends Model<CreateUserDto, User> {
+  @ApiProperty({
+    example: 1,
+    description: 'Уникальный идентификатор пользователя',
+  })
+  @PrimaryKey
+  @AutoIncrement
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  id: number;
+
+  @ApiProperty({ example: 'john_doe', description: 'Имя пользователя' })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  username: string;
+
+  @ApiProperty({
+    example: 'john@example.com',
+    description: 'Электронная почта пользователя',
+  })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    unique: true,
+  })
+  email: string;
+
+  @ApiProperty({
+    example: '+1234567890',
+    description: 'Номер телефона пользователя',
+  })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  phone: string;
+
+  @ApiProperty({ example: 'password123', description: 'Пароль пользователя' })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  password: string;
+}
