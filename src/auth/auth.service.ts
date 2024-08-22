0fcf6f8c89ed2a40bc57126331fbff638239b83a
mrTebrom <mr.tebron@gmail.com>
2024-08-22 16:36:04 +0500

    update auth service for refresh token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,28 @@
 // src/auth/auth.service.ts
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { RegisterUserDto } from './dto/registr-auth.dto';
 import { UserService } from 'src/user/user.service';
 import { JwtService } from '@nestjs/jwt';
 import { LoginUserDto } from './dto/authorization-auth.dto';
 import { InjectModel } from '@nestjs/sequelize';
 import { Token } from './entity/auth.entity';
+import { User } from 'src/user/entity/user.entity';
 
 @Injectable()
 export class AuthService {
   constructor(
     private jwt: JwtService, // Сервис для работы с JWT
     private userService: UserService, // Сервис для работы с пользователями
-    @InjectModel(Token) private model: typeof Token, // модель токена
+    @InjectModel(Token)
+    private entity: typeof Token, // Модель для работы с рефреш токенами
   ) {}
 
   // Регистрация пользователя
   async register(dto: RegisterUserDto) {
     // Регистрация пользователя через UserService
     const user = await this.userService.registration(dto);
-    // Генерация JWT токена для зарегистрированного пользователя
-    return this.generation(user);
+    // Генерация JWT и рефреш токенов для зарегистрированного пользователя
+    return this.generateTokens(user);
   }
 
   // Авторизация пользователя
@@ -29,22 +31,57 @@ export class AuthService {
     const user = await this.userService.findLogin(
       dto.email || dto.phone || dto.username,
     );
-    // Генерация JWT токена для авторизованного пользователя
-    return await this.generation(user);
+    // Генерация JWT и рефреш токенов для авторизованного пользователя
+    return this.generateTokens(user);
   }
 
-  // Генерация JWT токена
-  async generation(payload: any) {
-    console.log(payload); // Вывод полезной информации о пользователе (для отладки)
+  // Генерация JWT и рефреш токенов
+  async generateTokens(user: User) {
     // Создание JWT токена с полезной нагрузкой
-    return await this.jwt.sign({
-      sub: payload.id, // ID пользователя
-      roles: payload.roles.map((item) => {
-        return item.value; // Преобразование ролей пользователя в массив значений
-      }),
-      username: payload.username, // Имя пользователя
-      phone: payload.phone, // Телефон пользователя
-      email: payload.email, // Email пользователя
+    const accessToken = await this.jwt.sign({
+      sub: user.id, // ID пользователя
+      roles: user.roles.map((role) => role.value), // Преобразование ролей пользователя в массив значений
+      username: user.username, // Имя пользователя
+      phone: user.phone, // Телефон пользователя
+      email: user.email, // Email пользователя
     });
+
+    // Генерация рефреш токена
+    const refreshToken = await this.jwt.sign(
+      { sub: user.id },
+      { expiresIn: '7d' }, // Рефреш токен может иметь более длительный срок действия
+    );
+
+    // Удаление старого рефреш токена для текущего пользователя
+    await this.entity.destroy({ where: { userId: user.id } });
+
+    // Создание нового рефреш токена
+    await this.entity.create({
+      userId: user.id,
+      refreshToken: refreshToken,
+      expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // Установка срока действия 7 дней
+    });
+
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  // Обновление токенов по рефреш токену
+  async refreshToken(oldRefreshToken: string) {
+    // Поиск токена в базе данных
+    const tokenRecord = await this.entity.findOne({
+      where: { refreshToken: oldRefreshToken },
+    });
+
+    // Проверка на наличие и срок действия рефреш токена
+    if (!tokenRecord || tokenRecord.expiresAt < new Date()) {
+      throw new NotFoundException('Неверный или просроченный рефреш токен'); // Использование NotFoundException
+    }
+
+    // Поиск пользователя по ID из рефреш токена
+    const user = await this.userService.findById(tokenRecord.userId);
+    return this.generateTokens(user);
   }
 }
