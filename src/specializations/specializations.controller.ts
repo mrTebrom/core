1fa3d52e576a44d69b70a91400d7bcce14b1ce7f
mrTebrom <mr.tebron@gmail.com>
2024-08-28 16:02:22 +0500

    controller specialization

--- src/specializations/specializations.controller.ts
+++ src/specializations/specializations.controller.ts
@@ -10,38 +10,39 @@ import {
 import { SpecializationsService } from './specializations.service';
 import { CreateSpecializationDto } from './dto/create-specialization.dto';
 import { UpdateSpecializationDto } from './dto/update-specialization.dto';
+import { SpecializationsPipe } from './specializations.pipe';
 
 @Controller('specializations')
 export class SpecializationsController {
-  constructor(
-    private readonly specializationsService: SpecializationsService,
-  ) {}
+  constructor(private readonly service: SpecializationsService) {}
 
   @Post()
-  create(@Body() createSpecializationDto: CreateSpecializationDto) {
-    return this.specializationsService.create(createSpecializationDto);
+  create(
+    @Body(SpecializationsPipe) createSpecializationDto: CreateSpecializationDto,
+  ) {
+    return this.service.create(createSpecializationDto);
   }
 
   @Get()
   findAll() {
-    return this.specializationsService.findAll();
+    return this.service.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.specializationsService.findOne(+id);
+    return this.service.findOne(+id);
   }
 
   @Patch(':id')
   update(
     @Param('id') id: string,
-    @Body() updateSpecializationDto: UpdateSpecializationDto,
+    @Body(SpecializationsPipe) updateSpecializationDto: UpdateSpecializationDto,
   ) {
-    return this.specializationsService.update(+id, updateSpecializationDto);
+    return this.service.update(+id, updateSpecializationDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.specializationsService.remove(+id);
+    return this.service.remove(+id);
   }
 }
